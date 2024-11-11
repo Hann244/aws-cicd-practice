7dd709921ee1a97890bf583fa180550985ebefa1
EC2 Default User <ec2-user@ip-172-31-43-193.ap-northeast-2.compute.internal>
2024-11-11 05:00:41 +0000

    fixed : text delete

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "Hello Elice!!!!!! hi~ this is cloud4";
+    return "Hello Elice!";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
